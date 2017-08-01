24f2d37aace9e6a9df6c8f9b9d0bbb958f217d6f
Jeanne <jeanne.tiramani@hotmail.fr>
2017-08-01 19:15:04 +0200

    js fonctionnem

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('have fun !');
+
+$('#valider').click(function() {
+	
+	var objet = {
+				first_name:	 $('#first_name').val(),
+				last_name:	$('#last_name').val(),
+				city:	$('#city').val()
+	};
+
+console.log(objet);	
+$('#username').html(objet.first_name + ' ' + objet.last_name + ' de ' + objet.city );
+});
+
+
